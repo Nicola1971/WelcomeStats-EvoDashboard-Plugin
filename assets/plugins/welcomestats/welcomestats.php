f6b3ccc3d58b982491fde8cb06f18e33847d28fc
Nicola <modxit@modx.it>
2016-06-22 19:22:20 +0200

    3.0 version

--- assets/plugins/welcomestats/welcomestats.php
+++ assets/plugins/welcomestats/welcomestats.php
@@ -1,11 +1,23 @@
 <?php
 /**
- * WelcomeStats RC 2.0
+ * WelcomeStats RC 3.0
  *
- * Dashboard MODX Stats widget plugin for OnManagerWelcomeCustom
+ * Dashboard MODX Stats widget plugin for EvoDashboard
  * Event: OnManagerWelcomeHome,OnManagerWelcomeRender,OnManagerMainFrameHeaderHTMLBlock
- * Configuration: &StatsEvent= System Event:;list;OnManagerWelcomeHome,OnManagerWelcomeRender;OnManagerWelcomeHome &StatsBoxSize= Stats Box size:;list;dashboard-block-full,dashboard-block-half;dashboard-block-full &MODxStatsTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box
+ * Configuration: &WidgetTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box &datarow=widget row position:;list;1,2,3,4,5,6,7,8,9,10;1 &datacol=widget col position:;list;1,2,3,4;1 &datasizex=widget x size:;list;1,2,3,4;4 &datasizey=widget y size:;list;1,2,3,4,5,6,7,8,9,10;3
  */
+/*widget name*/
+$WidgetID = isset($WidgetID) ? $WidgetID : 'LinksBox';
+// size and position
+$datarow = isset($datarow) ? $datarow : '1';
+$datacol = isset($datacol) ? $datacol : '2';
+$datasizex = isset($datasizex) ? $datasizex : '2';
+$datasizey = isset($datasizey) ? $datasizey : '2';
+//output
+$WidgetOutput = isset($WidgetOutput) ? $WidgetOutput : '';
+//events
+$EvoEvent = isset($EvoEvent) ? $EvoEvent : 'OnManagerWelcomeHome';
+
 
 $StatsBoxSize = isset($StatsBoxSize) ? $StatsBoxSize : 'dashboard-block-full';
 $StatsContainerSize = isset($StatsContainerSize) ? $StatsContainerSize : 'col-sm-6';
@@ -61,17 +73,35 @@ if($e->name == 'OnManagerMainFrameHeaderHTMLBlock') {
     if ($Style == round) {$cssOutput = '<link type="text/css" rel="stylesheet" href="../assets/plugins/welcomestats/round.css">';}
     if ($Style == lite) {$cssOutput = '<link type="text/css" rel="stylesheet" href="../assets/plugins/welcomestats/lite.css">';}
 }
-if($e->name == ''.$StatsEvent.'') {
+if($e->name == ''.$EvoEvent.'') {
 	if ($Style == box) {$StatsContainerSize = 'col-sm-12';}
-   $Statsoutput = '<div class="'.$StatsBoxWidth.'"><div class="widget-wrapper"> <div class="widget-title sectionHeader"><i class="fa fa-bar-chart-o"></i> '.$MODxStatsTitle.'</div><div class="widget-stage sectionBody"><div class="statcontainer '.$StatsContainerSize.'">
-<div class="col-md-3 col-sm-6"><div class="statbox sblue"><div class="staticon"><i class="fa fa-file fa-4x"></i></div><div class="count"><h3> '.$num.' </h3> '.$DocCountLabel.' </div></div> </div>
+/*Widget Box */
+
+$WidgetOutput = '
+<li id="'.$WidgetID.'" data-row="'.$datarow.'" data-col="'.$datacol.'" data-sizex="'.$datasizex.'" data-sizey="'.$datasizey.'">
+                    <div class="panel panel-default widget-wrapper">
+                      <div class="panel-headingx widget-title sectionHeader clearfix">
+                          <span class="pull-left"><i class="fa fa-bar-chart-o"></i> '.$WidgetTitle.'</span>
+                            <div class="widget-controls pull-right">
+                                <div class="btn-group">
+                                    <a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="'.$WidgetID.'"></a>
+                                </div>     
+                            </div>
+
+                      </div>
+                      <div class="panel-body widget-stage sectionBody">
+                      <div class="col-md-3 col-sm-6"><div class="statbox sblue"><div class="staticon"><i class="fa fa-file fa-4x"></i></div><div class="count"><h3> '.$num.' </h3> '.$DocCountLabel.' </div></div> </div>
 <div class="col-md-3 col-sm-6"><div class="statbox sgreen"><div class="staticon"><i class="fa fa-users fa-4x"></i></div><div class="count"><h3> '.$count.' </h3> '.$WebUserCountLabel.' </div></div> </div>
 <div class="col-md-3 col-sm-6"><div class="statbox syellow"><div class="staticon"><i class="fa fa-user fa-4x"></i></div><div class="count"><h3> '.$userscount.' </h3> '.$UserCountLabel.' </div></div> </div>
 <div class="col-md-3 col-sm-6"><div class="statbox sred"><div class="staticon"><i class="fa fa-user-md fa-4x"></i></div><div class="count"><h3> '.$admincount.' </h3> '.$AdminCountLabel.' </div></div> </div>
-<div class="clear"></div> </div>
-	</div></div></div>';
+                      </div>
+                    </div>           
+                </li>
+
+';
+
 }
-$output .= $cssOutput.$Statsoutput;
+$output .= $cssOutput.$WidgetOutput;
 $e->output($output);
 return;
 ?>
--- install/assets/plugins/WelcomeStats.tpl
+++ install/assets/plugins/WelcomeStats.tpl
@@ -6,20 +6,20 @@
  *
  * @author    Nicola Lambathakis
  * @category    plugin
- * @version    2.1 RC
+ * @version    3.0 RC
  * @license	 http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
- * @internal    @events OnManagerWelcomePrerender,OnManagerWelcomeHome,OnManagerWelcomeRender,OnManagerMainFrameHeaderHTMLBlock
+ * @internal    @events OnManagerWelcomeHome,OnManagerMainFrameHeaderHTMLBlock
  * @internal    @installset base
  * @internal    @modx_category Dashboard
- * @internal    @properties  &StatsEvent= System Event:;list;OnManagerWelcomePrerender,OnManagerWelcomeHome,OnManagerWelcomeRender;OnManagerWelcomePrerender &StatsBoxSize= Stats Box size:;list;dashboard-block-full,dashboard-block-half;dashboard-block-full &MODxStatsTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box
+ * @internal    @properties  &WidgetTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box &datarow=widget row position:;list;1,2,3,4,5,6,7,8,9,10;1 &datacol=widget col position:;list;1,2,3,4;1 &datasizex=widget x size:;list;1,2,3,4;4 &datasizey=widget y size:;list;1,2,3,4,5,6,7,8,9,10;
  */
 
 /**
- * WelcomeStats RC 2.1
+ * WelcomeStats RC 3.0
  *
- * Dashboard MODX Stats widget plugin for OnManagerWelcomeCustom
- * Event: OnManagerWelcomeHome,OnManagerWelcomeRender,OnManagerMainFrameHeaderHTMLBlock
- * Configuration: &StatsEvent= System Event:;list;OnManagerWelcomePrerender,OnManagerWelcomeHome,OnManagerWelcomeRender;OnManagerWelcomePrerender &StatsBoxSize= Stats Box size:;list;dashboard-block-full,dashboard-block-half;dashboard-block-full &MODxStatsTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box
+ * Dashboard MODX Stats widget plugin for EvoDashboard
+ * Event: OnManagerWelcomeHome,OnManagerMainFrameHeaderHTMLBlock
+ * Configuration:  &WidgetTitle= MODx Stats Title:;string;MODx Stats  &DocCountLabel= Documents count label:;string;Documents &startID= Documents count parent:;string;0 &WebUserCountLabel= Web Users label:;string;Web Users &webGroup= Users Web Group:;string;all &UserCountLabel= Manager Users label:;string;Manager Users &AdminCountLabel= Admin label:;string;Admins &Style= Style:;list;box,round,lite;box &datarow=widget row position:;list;1,2,3,4,5,6,7,8,9,10;1 &datacol=widget col position:;list;1,2,3,4;1 &datasizex=widget x size:;list;1,2,3,4;4 &datasizey=widget y size:;list;1,2,3,4,5,6,7,8,9,10;3
  */
 
 // Run the main code
